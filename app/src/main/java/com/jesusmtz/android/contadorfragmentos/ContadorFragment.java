e7c0ae42078f063aa8a7b4893f38210be976cc21
Jesus Martinez Garcia <13470178@itcampeche.edu.mx>
2017-12-13 22:23:29 -0600

    ContadorFragmento: el fragmento de interfaz de usuario.

---
+++ app/src/main/java/com/jesusmtz/android/contadorfragmentos/ContadorFragment.java
@@ -0,0 +1,28 @@
+package com.jesusmtz.android.contadorfragmentos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Equipo on 13/12/2017.
+ */
+
+public class ContadorFragment extends Fragment{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragmento_contador, container, false);
+
+        return v;
+    }
+}
